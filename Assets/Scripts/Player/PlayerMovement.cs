ea0f00a3781a1494132e902f2d4ce499b4fc012f
Constant <constantsuchet@gmail.com>
2025-11-22 09:20:18 +0100

    Added player movement by using the new Input System of unity and fishnet transform

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,16 +1,40 @@
+using FishNet.Object;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+// Inherit from NetworkBehaviour instead of MonoBehaviour
+public class PlayerMovement : NetworkBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float moveSpeed = 5f;
+    private PlayerControls _playerControls;
+
+    private void Awake()
     {
-        
+        _playerControls = new PlayerControls();
     }
 
-    // Update is called once per frame
+    private void OnEnable()
+    {
+        _playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerControls.Disable();
+    }
+
     void Update()
     {
-        
+        // Only run this code on the object the local client owns.
+        // This prevents us from moving other players' objects.
+        if (!IsOwner)
+            return;
+
+        Vector2 input = _playerControls.Player.Move.ReadValue<Vector2>();
+        Vector3 moveDirection = new Vector3(input.x, 0f, input.y);
+
+        if (moveDirection.magnitude > 1f)
+            moveDirection.Normalize();
+
+        transform.position += moveSpeed * Time.deltaTime * moveDirection;
     }
 }
